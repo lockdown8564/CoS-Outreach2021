306f6e9d6bc40b65f960cc33e32555d480673e9d
LAPTOP-QLHBG3BL\16262 <thepsqq@gmail.com>
2020-12-16 22:15:49 -0800

    Merge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous.java
@@ -33,5 +33,130 @@ public class autonomous extends LinearOpMode {
 
         phoneCam.stopStreaming();
     }
+
+
+    /**
+     * our alliance for each match
+     */
+    private enum Alliance {
+        /**
+         * blue alliance side
+         */
+        BLUE,
+
+        /**
+         * red alliance side
+         */
+        RED
+    }
+
+    private enum StartingPos {
+        /**
+         * foundation side one tile away from the side wall
+         */
+        REDLEFT,
+
+        /**
+         * stone side along the side wall
+         */
+        REDRIGHT,
+
+        /**
+         * stone side one tile away from the side wall
+         */
+        BLUELEFT,
+
+        BLUERIGHT
+    }
+
+    private enum RingPosition {
+        FOUR,
+        ONE,
+        NONE
+    }
+    // need team string
+    // need startposition
+    // need to place
+    // need
+    switch(team)
+
+    {
+        case RED: {
+            switch (startposition) {
+                case REDLEFT: {
+                    switch (position) {
+                        case FOUR: {
+                            toC();
+                            break;
+                        }
+                        case ONE: {
+                            toB();
+                            break;
+                        }
+                        case NONE: {
+                            toA();
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case REDRIGHT: {
+
+                }
+
+                break;
+            }
+
+        }
+        case BLUE: {
+            switch (startposition) {
+                case BLUELEFT: {
+                    break;
+                }
+                case BLUERIGHT: {
+                    break;
+                }
+
+            }
+
+        }
+
+    }
+
+
+    //functions to get the robot to the spot
+    public static void toA() {
+        //align code here
+        frontLeftDrive.setPower(1);
+        frontRightDrive.setPower(1);
+        backLeftDrive.setPower(1);
+        backRightDrive.setPower(1);
+        sleep();
+        //place wobble code here
+    }
+
+    public static void toB() {
+        //align
+        frontLeftDrive.setPower(1);
+        frontRightDrive.setPower(1);
+        backLeftDrive.setPower(1);
+        backRightDrive.setPower(1);
+        sleep();
+        //place wobble code here
+
+
+    }
+
+    public static void toC() {
+        //align
+        frontLeftDrive.setPower(1);
+        frontRightDrive.setPower(1);
+        backLeftDrive.setPower(1);
+        backRightDrive.setPower(1);
+        sleep();
+        //place wobble code here
+
+    }
+}
     // I love Source Tree!
 }
