f845c1f62688ead48aafdb9e8075ee95f9d50d07
Kyla Penamante <kypenamante@gmail.com>
2021-03-24 13:11:07 -0700

    auto updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pidController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pidController.java
@@ -58,101 +58,102 @@ public class pidController extends LinearOpMode {
 //        robot.setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
     }
-    private void pidDrive(double speed, double FlInches, double FrInches, double BlInches, double BrInches){
-        int flTarget, frTarget, blTarget, brTarget;
-        if(opModeIsActive()){
-//            double flPower =
-            flTarget = (int)(FlInches*robot.getCountsPerInch()) + robot.frontLeftDrive.getCurrentPosition();
-            frTarget = (int)(FrInches*robot.getCountsPerInch()) + robot.frontRightDrive.getCurrentPosition();
-            blTarget = (int)(BlInches*robot.getCountsPerInch()) + robot.backLeftDrive.getCurrentPosition();
-            brTarget = (int)(BrInches*robot.getCountsPerInch()) + robot.backRightDrive.getCurrentPosition();
-
-            // set target positions & power of each motor
-            robot.frontLeftDrive.setTargetPosition(flTarget);
-            robot.frontRightDrive.setTargetPosition(frTarget);
-            robot.backLeftDrive.setTargetPosition(blTarget);
-            robot.backRightDrive.setTargetPosition(brTarget);
-
-            robot.setDriveMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            FlPidDrive(flTarget);
-            FrPidDrive(frTarget);
-            BlPidDrive(blTarget);
-//            BrPidDrive(brTarget);
-            pidRuntime.reset();
-//          robot.verticalDriveSetTarget(LEFT_TARGET, RIGHT_TARGET);
-            robot.stopAllMotorPower();
-            robot.setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-    }
-
-    public void FlPidDrive(/*double speed, */ double targetPos) {
-        double integral = 0;
-        ElapsedTime FlPidRuntime = new ElapsedTime();
-        while(opModeIsActive()) {
-            double error = robot.frontLeftDrive.getCurrentPosition();
-            double lastError = 0;
-            while (Math.abs(error) <= 9 && opModeIsActive()/* && repetitions < whatever number */) {
-                error = robot.frontLeftDrive.getCurrentPosition() - targetPos;
-                double changeInError = lastError - error;
-                integral += changeInError * FlPidRuntime.time();
-                double derivative = changeInError / FlPidRuntime.time();
-                double P = pidCoeffs.p * error;
-                double I = pidCoeffs.i * integral;
-                double D = pidCoeffs.d * derivative;
-                //maybe set target pos at top of this function
-                //maybe try troubleshooting by rearranging the setPower commands
-                robot.frontLeftDrive.setPower(/*(Math.abs(speed)) * */(P + I + D));
-                error = lastError;
-                repCount++;
-                FlPidRuntime.reset();
-            }
-            telemetry.addData("error: ", error);
-            telemetry.update();
-        }
-    }
-
-    public void FrPidDrive(/*double speed,*/ double targetPos) {
-        double integral = 0;
-        ElapsedTime FrPidRuntime = new ElapsedTime();
-        while(opModeIsActive()) {
-            double error = robot.frontRightDrive.getCurrentPosition();
-            double lastError = 0;
-            while (Math.abs(error) <= 9 && opModeIsActive()/* && repetitions < whatever number */) {
-                error = robot.frontRightDrive.getCurrentPosition() - targetPos;
-                double changeInError = lastError - error;
-                integral += changeInError * FrPidRuntime.time();
-                double derivative = changeInError / FrPidRuntime.time();
-                double P = pidCoeffs.p * error;
-                double I = pidCoeffs.i * integral;
-                double D = pidCoeffs.d * derivative;
-                robot.frontRightDrive.setPower(/*(Math.abs(speed)) * */(P + I + D));
-                error = lastError;
-                FrPidRuntime.reset();
-            }
-        }
-    }
-
-    public void BlPidDrive(/*double speed,*/ double targetPos) {
-        double integral = 0;
-        ElapsedTime BlPidRuntime = new ElapsedTime();
-        while(opModeIsActive()) {
-            double error = robot.backLeftDrive.getCurrentPosition();
-            double lastError = 0;
-            while (Math.abs(error) <= 9 && opModeIsActive()/* && repetitions < whatever number */) {
-                error = robot.backLeftDrive.getCurrentPosition() - targetPos;
-                double changeInError = lastError - error;
-                integral += changeInError * BlPidRuntime.time();
-                double derivative = changeInError / BlPidRuntime.time();
-                double P = pidCoeffs.p * error;
-                double I = pidCoeffs.i * integral;
-                double D = pidCoeffs.d * derivative;
-                robot.backLeftDrive.setPower(/*(Math.abs(speed)) * */(P + I + D));
-                error = lastError;
-                BlPidRuntime.reset();
-            }
-        }
-    }
+    // ignore
+//    private void pidDrive(double speed, double FlInches, double FrInches, double BlInches, double BrInches){
+//        int flTarget, frTarget, blTarget, brTarget;
+//        if(opModeIsActive()){
+////            double flPower =
+//            flTarget = (int)(FlInches*robot.getCountsPerInch()) + robot.frontLeftDrive.getCurrentPosition();
+//            frTarget = (int)(FrInches*robot.getCountsPerInch()) + robot.frontRightDrive.getCurrentPosition();
+//            blTarget = (int)(BlInches*robot.getCountsPerInch()) + robot.backLeftDrive.getCurrentPosition();
+//            brTarget = (int)(BrInches*robot.getCountsPerInch()) + robot.backRightDrive.getCurrentPosition();
+//
+//            // set target positions & power of each motor
+//            robot.frontLeftDrive.setTargetPosition(flTarget);
+//            robot.frontRightDrive.setTargetPosition(frTarget);
+//            robot.backLeftDrive.setTargetPosition(blTarget);
+//            robot.backRightDrive.setTargetPosition(brTarget);
+//
+//            robot.setDriveMode(DcMotor.RunMode.RUN_TO_POSITION);
+//
+//            FlPidDrive(flTarget);
+//            FrPidDrive(frTarget);
+//            BlPidDrive(blTarget);
+////            BrPidDrive(brTarget);
+//            pidRuntime.reset();
+////          robot.verticalDriveSetTarget(LEFT_TARGET, RIGHT_TARGET);
+//            robot.stopAllMotorPower();
+//            robot.setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        }
+//    }
+//
+//    public void FlPidDrive(/*double speed, */ double targetPos) {
+//        double integral = 0;
+//        ElapsedTime FlPidRuntime = new ElapsedTime();
+//        while(opModeIsActive()) {
+//            double error = robot.frontLeftDrive.getCurrentPosition();
+//            double lastError = 0;
+//            while (Math.abs(error) <= 9 && opModeIsActive()/* && repetitions < whatever number */) {
+//                error = robot.frontLeftDrive.getCurrentPosition() - targetPos;
+//                double changeInError = lastError - error;
+//                integral += changeInError * FlPidRuntime.time();
+//                double derivative = changeInError / FlPidRuntime.time();
+//                double P = pidCoeffs.p * error;
+//                double I = pidCoeffs.i * integral;
+//                double D = pidCoeffs.d * derivative;
+//                //maybe set target pos at top of this function
+//                //maybe try troubleshooting by rearranging the setPower commands
+//                robot.frontLeftDrive.setPower(/*(Math.abs(speed)) * */(P + I + D));
+//                error = lastError;
+//                repCount++;
+//                FlPidRuntime.reset();
+//            }
+//            telemetry.addData("error: ", error);
+//            telemetry.update();
+//        }
+//    }
+//
+//    public void FrPidDrive(/*double speed,*/ double targetPos) {
+//        double integral = 0;
+//        ElapsedTime FrPidRuntime = new ElapsedTime();
+//        while(opModeIsActive()) {
+//            double error = robot.frontRightDrive.getCurrentPosition();
+//            double lastError = 0;
+//            while (Math.abs(error) <= 9 && opModeIsActive()/* && repetitions < whatever number */) {
+//                error = robot.frontRightDrive.getCurrentPosition() - targetPos;
+//                double changeInError = lastError - error;
+//                integral += changeInError * FrPidRuntime.time();
+//                double derivative = changeInError / FrPidRuntime.time();
+//                double P = pidCoeffs.p * error;
+//                double I = pidCoeffs.i * integral;
+//                double D = pidCoeffs.d * derivative;
+//                robot.frontRightDrive.setPower(/*(Math.abs(speed)) * */(P + I + D));
+//                error = lastError;
+//                FrPidRuntime.reset();
+//            }
+//        }
+//    }
+//
+//    public void BlPidDrive(/*double speed,*/ double targetPos) {
+//        double integral = 0;
+//        ElapsedTime BlPidRuntime = new ElapsedTime();
+//        while(opModeIsActive()) {
+//            double error = robot.backLeftDrive.getCurrentPosition();
+//            double lastError = 0;
+//            while (Math.abs(error) <= 9 && opModeIsActive()/* && repetitions < whatever number */) {
+//                error = robot.backLeftDrive.getCurrentPosition() - targetPos;
+//                double changeInError = lastError - error;
+//                integral += changeInError * BlPidRuntime.time();
+//                double derivative = changeInError / BlPidRuntime.time();
+//                double P = pidCoeffs.p * error;
+//                double I = pidCoeffs.i * integral;
+//                double D = pidCoeffs.d * derivative;
+//                robot.backLeftDrive.setPower(/*(Math.abs(speed)) * */(P + I + D));
+//                error = lastError;
+//                BlPidRuntime.reset();
+//            }
+//        }
+//    }
 
     public void PidDrive(double speed, double distance) {
         repCount = 0;
